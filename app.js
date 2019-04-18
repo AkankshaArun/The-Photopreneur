b02ffe406a56c4e59a8efeb2de7eb4b4580131bb
AkankshaArun <akanksha.arun26@gmail.com>
2019-04-18 15:43:35 +0530

    adding contact page

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get("/about",function(req,res){
   res.render("about");
 });
 
+app.get("/contact",function(req,res){
+  res.render("contact");
+});
+
 app.listen("3000",function(){
   console.log("Server Is Responding")
 });
