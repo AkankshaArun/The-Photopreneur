b6f39dcd99079957c40335b05a5392c876a3e5b9
AkankshaArun <akanksha.arun26@gmail.com>
2019-04-23 20:29:30 +0530

    modified carousel

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var User = require("../models/user");
 // var nodemailer = require("nodemailer");
 // var crypto = require("crypto");
 
-//root route
 //Rendering Index
 router.get("/",function(req,res){
   res.render("index");
@@ -62,62 +61,62 @@ router.get("/logout", function(req, res){
    res.redirect("/");
 });
 
-// forgot password
-router.get('/forgot', function(req, res) {
-  res.render('forgot');
-});
-
-router.post('/forgot', function(req, res, next) {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'No account with that email address exists.');
-          return res.redirect('/forgot');
-        }
-
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: 'photopreneur.horizon@gmail.com',
-          pass: process.env.GMAILPW
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'photopreneur.horizon@gmail.com',
-        subject: 'The Photopreneur Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log('mail sent');
-        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    res.redirect('/forgot');
-  });
-});
+// // forgot password
+// router.get('/forgot', function(req, res) {
+//   res.render('forgot');
+// });
+//
+// router.post('/forgot', function(req, res, next) {
+//   async.waterfall([
+//     function(done) {
+//       crypto.randomBytes(20, function(err, buf) {
+//         var token = buf.toString('hex');
+//         done(err, token);
+//       });
+//     },
+//     function(token, done) {
+//       User.findOne({ email: req.body.email }, function(err, user) {
+//         if (!user) {
+//           req.flash('error', 'No account with that email address exists.');
+//           return res.redirect('/forgot');
+//         }
+//
+//         user.resetPasswordToken = token;
+//         user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+//
+//         user.save(function(err) {
+//           done(err, token, user);
+//         });
+//       });
+//     },
+//     function(token, user, done) {
+//       var smtpTransport = nodemailer.createTransport({
+//         service: 'Gmail',
+//         auth: {
+//           user: 'photopreneur.horizon@gmail.com',
+//           pass: process.env.GMAILPW
+//         }
+//       });
+//       var mailOptions = {
+//         to: user.email,
+//         from: 'photopreneur.horizon@gmail.com',
+//         subject: 'The Photopreneur Password Reset',
+//         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+//           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+//           'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+//           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+//       };
+//       smtpTransport.sendMail(mailOptions, function(err) {
+//         console.log('mail sent');
+//         req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+//         done(err, 'done');
+//       });
+//     }
+//   ], function(err) {
+//     if (err) return next(err);
+//     res.redirect('/forgot');
+//   });
+// });
 
 //middleware
 function isLoggedIn(req, res, next){
