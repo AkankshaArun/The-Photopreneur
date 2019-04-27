2125791cf27449b232b503059c97644002950c84
Gagandeep Singh <gagandeepsingh1410@gmail.com>
2019-04-27 12:48:11 +0530

    moving files

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/photopreneur",{useNewUrlParser: true});
 var indexRoutes = require("./routes/index");
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Once again Rusty wins cutest dog!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();});
 app.get("/categories",function(req,res){
   Photo.find({},function(err, photos){
     if (err) {
@@ -73,23 +90,6 @@ app.get("/categories/:id",function(req,res){
   })
 });
 
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();});
 
     // passport.use(new GoogleStrategy({
     //     clientID: "636201172967-2gciiaoqpq9u4vk82b5r0is2m8u4jp02.apps.googleusercontent.com",
