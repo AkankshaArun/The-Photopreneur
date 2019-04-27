34e8694500fc2a834f814b45747e34d340f31838
Gagandeep Singh <gagandeepsingh1410@gmail.com>
2019-04-27 10:39:02 +0530

    Completing Category Page

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
+let Comment = require("./modals/comment");
+let Photo = require("./modals/photo");
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+let bodyParser = require("body-parser");
+
 app.set("view engine","ejs");
 app.use(express.static("public"));
 let mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/photo",{useNewUrlParser: true});
-//schema setup
-var schema = new mongoose.Schema({
-imgURL: String,
-tag: String,
-description: String
-});
-//model setup
-var photo = new mongoose.model("photo",schema);
-//Adding photo manully
+app.use(bodyParser.urlencoded({extended: true}));
 
-/*photo.create({
-  imgURL : "/images/home/RandomH.JPG",
+mongoose.connect("mongodb://localhost:27017/photo",{useNewUrlParser: true});
+
+//schema setup
+
+//Adding photo manully
+/*Photo.create({
+  imgURL : "/images/home/PeopleH.JPG",
+  tag : "people",
+  discription: "This is a People Image"
+
+},function(err,added){
+  if (err) {
+    console.log(err);
+  }else {
+    console.log(added);
+  }
+});
+Photo.create({
+  imgURL : "/images/home/RandomH.jpg",
   tag : "random",
   discription: "This is a random Image"
 
@@ -27,7 +39,32 @@ var photo = new mongoose.model("photo",schema);
     console.log(added);
   }
 });
+Photo.create({
+  imgURL : "/images/home/FloraH.jpg",
+  tag : "Flora & Fauna",
+  discription: "This is a Flora & Fauna Image"
+
+},function(err,added){
+  if (err) {
+    console.log(err);
+  }else {
+    console.log(added);
+  }
+});
+Photo.create({
+  imgURL : "/images/home/FoodH.jpg",
+  tag : "food",
+  discription: "This is a food Image"
+
+},function(err,added){
+  if (err) {
+    console.log(err);
+  }else {
+    console.log(added);
+  }
+});
 */
+
 //adding image to the website
 /*app.post("/categories",function(req,res){
   var imgURL = req.body.name;
@@ -46,7 +83,7 @@ var photo = new mongoose.model("photo",schema);
 */
 //Sending data to the page
 app.get("/categories",function(req,res){
-  photo.find({},function(err, photos){
+  Photo.find({},function(err, photos){
     if (err) {
       console.log(err);
     }else {
@@ -54,16 +91,53 @@ app.get("/categories",function(req,res){
     }
   });
 });
+/*==========================Akanksha======================================*/
+/*function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+  return next();
 
-
+}*/
+/*==========================Akanksha======================================*/
 //Rendering Index
 app.get("/",function(req,res){
   res.render("index");
 });
 
+app.post("/categories/:id/comment",function(req,res){
+  Photo.findById(req.params.id,function(error,photo){
+    if (error) {
+      console.log(error);
+      res.redirect("/categories");
+    }else{
+      Comment.create(req.body.comment, function(err,commentData){
+        if (err) {
+          console.log(err);
+        }else {
+          //commentData.author.username = req.user.username;
+          //.author._id = req.user._id;
+          photo.comments.push(commentData);
+          console.log(photo);
+          photo.save();
+          console.log(commentData);
+          res.redirect("/categories/" + photo._id);
+        }
+      })
+    }
+  })
+});
 
-app.get("/categories",function(req,res){
-  res.render("categories");
+//rendering Comment
+app.get("/categories/:id",function(req,res){
+  Photo.findById(req.params.id).populate("comments").exec(function(err,photo){
+    if (err) {
+      console.log(err);
+    }else {
+      res.render("show", {photo: photo});
+    }
+  })
 });
 
 app.get("/about",function(req,res){
